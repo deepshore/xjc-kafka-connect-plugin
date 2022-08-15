e072149241e46171d944ea6756d42aed5ef15ea4
Selamanse <selamanse@scheinfrei.info>
2022-08-15 16:25:04 +0200

    Add custom release mechanism

--- bin/mvn-deploy-github.sh
+++ bin/mvn-deploy-github.sh
@@ -2,9 +2,15 @@
 
 MVN_SETTINGS_TEMPLATE_FILE=.mvn/settings.xml.tmpl
 MVN_SETTINGS_FILE=.mvn/settings.xml
+POM_FILE=pom.xml
 
 sed -e "s/GH_REPO/${GH_REPO}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
 sed -e "s/GH_DEPLOY_USERNAME/${GH_DEPLOY_USERNAME}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
 sed -e "s/GH_DEPLOY_TOKEN/${GH_DEPLOY_TOKEN}/g" ${MVN_SETTINGS_TEMPLATE_FILE} > ${MVN_SETTINGS_FILE}
+
+if [[ $GITHUB_REF_NAME == v* ]]; then
+  echo "creating release version $GITHUB_REF_NAME"
+  sed -i -e "s%\<version>0\.2-SNAPSHOT<\/version>%<version>${GITHUB_REF_NAME}</version>%g" ${POM_FILE}
+fi
 
 mvn -s ${MVN_SETTINGS_FILE} --batch-mode deploy -Dlicense.skip=true
