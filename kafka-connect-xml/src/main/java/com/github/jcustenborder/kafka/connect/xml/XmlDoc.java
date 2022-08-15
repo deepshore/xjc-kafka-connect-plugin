cd70344cc0c0b2ba3cca2335d56e8d9cb907b5dd
Selamanse <selamanse@scheinfrei.info>
2022-08-15 16:04:53 +0200

    Add custom xsd annotations part 1

---
+++ kafka-connect-xml/src/main/java/com/github/jcustenborder/kafka/connect/xml/XmlDoc.java
@@ -0,0 +1,12 @@
+package com.github.jcustenborder.kafka.connect.xml;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+public @interface XmlDoc {
+    String documentation() default "";
+}
